eb7406c9722ab4aaf9d41c60541511799bc2acee
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-11-15 11:40:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
    float a=0, b=0, x=0, y=0, z=0;
-   cin >> a << b << x << y;
+   cin >> a >> b >> x >> y;
    if (x < 0 && y > 0) {
       cout <<( z = (a * x) – (b * y))<< endl;
       }else if ( x >= 0 && y <= 0) {
